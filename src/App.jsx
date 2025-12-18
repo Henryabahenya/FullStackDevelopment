7a8dcc16f159672dbe3f63937e781d4b56cbf838
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-12-18 20:19:07 +0200

    Add sub-commit before part8-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useQuery } from '@apollo/client/react'
+import { useApolloClient, useQuery } from '@apollo/client/react'
 import { useState } from 'react'
+import LoginForm from './components/LoginForm'
 import Notify from './components/Notify'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -7,11 +8,19 @@ import PhoneForm from './components/PhoneForm'
 import { ALL_PERSONS } from './queries'
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS)
+  const client = useApolloClient()
 
   if (result.loading) {
     return <div>loading...</div>
+  }
+
+  const onLogout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
   }
 
   const notify = (message) => {
@@ -21,9 +30,20 @@ const App = () => {
     }, 10000)
   }
 
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    )
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={onLogout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import { ApolloClient, gql, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import { ApolloProvider } from '@apollo/client/react'
 
 const client = new ApolloClient({
@@ -10,24 +10,6 @@ const client = new ApolloClient({
     uri: 'http://localhost:4000',
   }),
   cache: new InMemoryCache(),
-})
-
-const query = gql`
-  query {
-    allPersons {
-      name
-      phone
-      address {
-        street
-        city
-      }
-      id
-    }
-  }
-`
-
-client.query({ query }).then((response) => {
-  console.log(response.data)
 })
 
 createRoot(document.getElementById('root')).render(

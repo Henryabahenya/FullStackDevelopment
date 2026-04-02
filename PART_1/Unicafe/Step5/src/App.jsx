c883bb7adc914fd28e95e190b0407a3c58719a45
Henry Abahenya <abahenyahadre.com>
2026-04-02 10:46:34 +0300

    feat: complete Part 1 Unicaf Step 5

--- PART_1/Unicafe/Step5/src/App.jsx
+++ PART_1/Unicafe/Step5/src/App.jsx
@@ -36,6 +36,7 @@ const Statistics = (props) => {
 
   return (
     <div>
+
       <h1>statistics</h1>
       <StatisticLine text="good" value={props.good} />
       <StatisticLine text="neutral" value={props.neutral} />
@@ -43,6 +44,7 @@ const Statistics = (props) => {
       <StatisticLine text="all" value={all} />
       <StatisticLine text="average" value={average} />
       <StatisticLine text="positive" value={positive + " %"} />
+   
     </div>
   )
 }
@@ -56,12 +58,14 @@ const App = () => {
 
   return (
     <div>
+
       <h1>give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
       <Statistics good={good} neutral={neutral} bad={bad} />
+   
     </div>
   )
 }

4d44096cfb618ec88ebeb85d0fabf2c5cc548fd0
Henry Abahenya <abahenyahadre.com>
2026-04-06 14:06:32 +0300

    Step 1: Add phonebook functionality. Implemented controlled inputs, form submission with preventDefault, and list rendering using .concat and map.

---
+++ PART_2/PhoneBook/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log('Typing:', event.target.value)
+    setNewName(event.target.value)
+  }
+
+ 
+  const addPerson = (event) => {
+    event.preventDefault() 
+    console.log('Button clicked, adding:', newName)
+
+    const nameObject = {
+      name: newName
+    }
+
+  
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+   
+ 
+
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <ul>
+      
+        {persons.map(person => 
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App

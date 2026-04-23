2696f7c358df88ecb3de635bbcc5eea706642e00
Henry Abahenya <abahenyahadre.com>
2026-04-23 13:27:34 +0300

    Phonebook Backend step8 completed

--- PART_3/PhoneBookBackend/index.js
+++ PART_3/PhoneBookBackend/index.js
@@ -1,28 +1,81 @@
 const express = require('express')
-const morgan = require('morgan') // 1. REQUIRE MORGAN
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny')) // 2. USE MORGAN WITH 'TINY' CONFIGURATION
+
+
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
-    { "id": "1", "name": "Arto Hellas", "number": "040-123456" },
-    { "id": "2", "name": "Ada Lovelace", "number": "39-44-5323523" },
-    { "id": "3", "name": "Dan Abramov", "number": "12-43-234345" },
-    { "id": "4", "name": "Mary Poppendieck", "number": "39-23-6423122" },
-    { "id": "5", "name": "Henry Abahenya", "number": "+254768407749" }
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    },
+    {
+      "id": "5",
+      "name": "Henry Abahenya",
+      "number": "+254768407749"
+    }
 ]
 
 
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+
+app.get('/info', (req, res) => {
+    const count = persons.length
+    const date = new Date()
+    const resText = `
+        <p>Phonebook has info for ${count} people</p>
+        <p>${date}</p>
+    `
+    res.send(resText)
+})
+
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).json({ error: 'person not found' })
+    }
+})
 
 app.delete('/api/persons/:id', (req, res) => {
     const id = req.params.id
-    persons = persons.filter(person => person.id !== id)
+    persons = persons.filter(p => p.id !== id)
     res.status(204).end()
 })
 
 app.post('/api/persons', (req, res) => {
     const body = req.body 
+
 
     if (!body.name || !body.number) {
         return res.status(400).json({ 
@@ -30,7 +83,7 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-   
+
     const nameExists = persons.find(p => p.name === body.name)
     if (nameExists) {
         return res.status(400).json({ 
@@ -41,6 +94,7 @@ app.post('/api/persons', (req, res) => {
     const person = {
         name: body.name,
         number: body.number,
+   
         id: String(Math.floor(Math.random() * 1000000)),
     }
 
@@ -48,7 +102,8 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
+
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
 })

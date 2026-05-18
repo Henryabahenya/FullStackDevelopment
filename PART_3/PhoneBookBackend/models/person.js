c307f7ad895266f97ffd17f0c610247137d91f4b
Henry Abahenya <abahenyahadre.com>
2026-05-18 09:51:16 +0300

    Phonebook database, step 8

--- PART_3/PhoneBookBackend/models/person.js
+++ PART_3/PhoneBookBackend/models/person.js
@@ -13,15 +13,30 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
+  
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 3,       
-    required: true       
+    minlength: 3,
+    required: true
   },
   number: {
     type: String,
-    required: true
+    required: true,
+    // 1. Enforce a minimum total length of 8 characters
+    minlength: [8, 'Phone number must have at least 8 characters'],
+    
+    // 2. Use a custom validator with a Regular Expression (Regex)
+    validate: {
+      validator: function(v) {
+        // Regex broken down:
+        // ^\d{2,3}   -> Starts with exactly 2 or 3 digits
+        // -          -> Followed by a single hyphen
+        // \d+$       -> Ends with one or more digits until the end of the string
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number! Format must be XX-XXXXXXX or XXX-XXXXXXX.`
+    }
   }
 })
 

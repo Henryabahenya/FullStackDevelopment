f23296d95c9e1c2a3902bd7b445377e75cd5cbf5
Henry Abahenya <abahenyahadre.com>
2026-05-19 16:10:36 +0300

    Helper Functions and Unit Tests, step 3

--- PART_4/blog-list/tests/list_helper.test.js
+++ PART_4/blog-list/tests/list_helper.test.js
@@ -2,40 +2,10 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-describe('list helpers', () => {
-  test('dummy returns one', () => {
-    const blogs = []
-    const result = listHelper.dummy(blogs)
-    assert.strictEqual(result, 1)
-  })
-})
 
-describe('total likes', () => {
-  test('of empty list is zero', () => {
-    const blogs = []
-    const result = listHelper.totalLikes(blogs)
-    assert.strictEqual(result, 0)
-  })
 
-  
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual(result, 5)
-  })
-
- 
-  const biggerList = [
+describe('favorite blog', () => {
+  const blogs = [
     {
       _id: '5a422a851b54a676234d17f7',
       title: 'React patterns',
@@ -62,9 +32,14 @@ describe('total likes', () => {
     }
   ]
 
-  test('of a bigger list is calculated right', () => {
+  test('returns the blog with the most likes', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    
    
-    const result = listHelper.totalLikes(biggerList)
-    assert.strictEqual(result, 24)
+    assert.deepStrictEqual(result, {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12
+    })
   })
 })
--- PART_4/blog-list/utils/list_helper.js
+++ PART_4/blog-list/utils/list_helper.js
@@ -3,13 +3,30 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  
-  return blogs.reduce((sum, blog) => {
-    return sum + blog.likes
-  }, 0)
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  
+  const favorite = blogs.reduce((max, blog) => {
+    return blog.likes > max.likes ? blog : max
+  }, blogs[0])
+
+ 
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }

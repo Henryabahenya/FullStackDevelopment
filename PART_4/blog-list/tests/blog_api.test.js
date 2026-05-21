6a3f8574e348a33efa98ec3687f6f9d226575d36
Henry Abahenya <abahenyahadre.com>
2026-05-21 13:41:34 +0300

    Blog List Expansions, step 1

--- PART_4/blog-list/tests/blog_api.test.js
+++ PART_4/blog-list/tests/blog_api.test.js
@@ -101,6 +101,22 @@ describe('blog api tests', () => {
       .expect(400)
   })
 
+  test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+  const titles = blogsAtEnd.map(b => b.title)
+  assert.ok(!titles.includes(blogToDelete.title))
+})
+
   after(async () => {
     await mongoose.connection.close()
   })

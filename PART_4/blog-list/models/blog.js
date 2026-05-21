7981a636d6814130d6a0965836e05e87e15a85f9
Henry Abahenya <abahenyahadre.com>
2026-05-21 13:01:27 +0300

    Blog List Tests, step 3

--- PART_4/blog-list/models/blog.js
+++ PART_4/blog-list/models/blog.js
@@ -9,13 +9,12 @@ const blogSchema = new mongoose.Schema({
 
 blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    // 1. Create a clean string 'id' from the MongoDB '_id' object
+
     returnedObject.id = returnedObject._id.toString()
-    
-    // 2. Remove the ugly internal database '_id'
+
     delete returnedObject._id
     
-    // 3. Remove the MongoDB versioning counter (__v)
+
     delete returnedObject.__v
   }
 })
--- PART_4/blog-list/tests/blog_api.test.js
+++ PART_4/blog-list/tests/blog_api.test.js
@@ -42,6 +42,32 @@ test('blogs have a unique identifier property named id', async () => {
   assert.strictEqual(firstBlog._id, undefined)
 })
 
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: 'Async/Await simplifies asynchronous code',
+    author: 'Henry Abahenya',
+    url: 'https://fullstackopen.com/',
+    likes: 12
+  }
+
+  // 1. Make the POST request using supertest
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  // 2. Fetch all blogs from the DB using our helper utility
+  const blogsAtEnd = await helper.blogsInDb()
+  
+  // 3. Verify total count increased by 1
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  // 4. Verify the title of the saved blog exists in the database
+  const titles = blogsAtEnd.map(b => b.title)
+  assert.ok(titles.includes('Async/Await simplifies asynchronous code'))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })

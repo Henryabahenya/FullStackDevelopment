ff58303778c50b660a0d0f1c18a39b5d90a83ed7
Henry Abahenya <abahenyahadre.com>
2026-05-28 11:24:31 +0300

    4.19: Blog List Expansion, step 7

--- PART_4/blog-list/tests/blog_api.test.js
+++ PART_4/blog-list/tests/blog_api.test.js
@@ -1,137 +1,89 @@
-const { test, describe, beforeEach, after } = require("node:test");
-const assert = require("node:assert");
-const supertest = require("supertest");
-const mongoose = require("mongoose");
-const app = require("../app");
-const api = supertest(app);
-const helper = require("./test_helper");
-const Blog = require("../models/blog");
-const User = require("../models/user");
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const User = require('../models/user')
 
-describe("blog api tests", () => {
+describe('when there is initially some blogs saved', () => {
+  let token // Variable to store our active test token
+
   beforeEach(async () => {
-    await Blog.deleteMany({});
-    await User.deleteMany({});
+    await Blog.deleteMany({})
+    await User.deleteMany({})
 
-    // Create a default seed user for the blogs to attach to
-    const defaultUser = new User({
-      username: "testuser",
-      passwordHash: "hashedpassword",
-    });
-    await defaultUser.save();
+    // 1. Create a baseline test user in the test database
+    const testUser = new User({
+      username: 'test_admin',
+      name: 'Test Admin',
+      passwordHash: 'hashed_password_placeholder' // Normally hashed via bcrypt
+    })
+    await testUser.save()
 
-    // Attach this user ID to your initial seed blogs array
-    const blogsWithUser = helper.initialBlogs.map((blog) => ({
-      ...blog,
-      user: defaultUser._id,
-    }));
+    // 2. Generate a valid token for this test user
+    const jwt = require('jsonwebtoken')
+    token = jwt.sign(
+      { username: testUser.username, id: testUser._id }, 
+      process.env.SECRET
+    )
 
-    await Blog.insertMany(blogsWithUser);
-  });
+    // 3. Seed initial blogs linked to this user
+    const initialBlogs = [
+      {
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+        user: testUser._id
+      }
+    ]
+    await Blog.insertMany(initialBlogs)
+  })
 
-  test("blogs have a unique identifier property named id", async () => {
-    const response = await api.get("/api/blogs");
-    const firstBlog = response.body[0];
-
-    assert.ok(firstBlog.id);
-    assert.strictEqual(firstBlog._id, undefined);
-  });
-
-  test("a valid blog can be added", async () => {
+  test('a valid blog can be added with a valid token', async () => {
     const newBlog = {
-      title: "Async/Await simplifies asynchronous code",
-      author: "Henry Abahenya",
-      url: "https://fullstackopen.com/",
-      likes: 12,
-    };
+      title: 'Testing Token Protected Routes',
+      author: 'Henry Abahenya',
+      url: 'https://fullstackopen.com/en/part4/token_authentication',
+      likes: 12
+    }
 
+    // Pass the token inside the Authorization header string layout
     await api
-      .post("/api/blogs")
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
       .send(newBlog)
       .expect(201)
-      .expect("Content-Type", /application\/json/);
+      .expect('Content-Type', /application\/json/)
 
-    const blogsAtEnd = await helper.blogsInDb();
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+    const blogsAtEnd = await Blog.find({})
+    assert.strictEqual(blogsAtEnd.length, 2)
+    
+    const titles = blogsAtEnd.map(b => b.title)
+    assert(titles.includes('Testing Token Protected Routes'))
+  })
 
-    const titles = blogsAtEnd.map((b) => b.title);
-    assert.ok(titles.includes("Async/Await simplifies asynchronous code"));
-  });
+  test('blog creation fails with status code 401 if token is not provided', async () => {
+    const newBlog = {
+      title: 'Unauthorized Blog Post',
+      author: 'Anonymous',
+      url: 'https://unauthorized.com',
+      likes: 0
+    }
 
-  test("if the likes property is missing, it defaults to 0", async () => {
-    const newBlogWithoutLikes = {
-      title: "Understanding Default Values in Mongoose",
-      author: "Henry Abahenya",
-      url: "https://fullstackopen.com/",
-    };
+    // Intentionally omit setting the Authorization header
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(401)
 
-    const response = await api
-      .post("/api/blogs")
-      .send(newBlogWithoutLikes)
-      .expect(201)
-      .expect("Content-Type", /application\/json/);
+    const blogsAtEnd = await Blog.find({})
+    assert.strictEqual(blogsAtEnd.length, 1) // Assert no blog was added
+  })
+})
 
-    assert.strictEqual(response.body.likes, 0);
-  });
-
-  test("blog without title is not added", async () => {
-    const newBlogWithoutTitle = {
-      author: "Henry Abahenya",
-      url: "https://fullstackopen.com/",
-      likes: 5,
-    };
-
-    await api.post("/api/blogs").send(newBlogWithoutTitle).expect(400);
-  });
-
-  test("blog without url is not added", async () => {
-    const newBlogWithoutUrl = {
-      title: "Testing Missing URL Validation",
-      author: "Henry Abahenya",
-      likes: 5,
-    };
-
-    await api.post("/api/blogs").send(newBlogWithoutUrl).expect(400);
-  });
-
-  test("a blog can be deleted", async () => {
-    const blogsAtStart = await helper.blogsInDb();
-    const blogToDelete = blogsAtStart[0];
-
-    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-    const blogsAtEnd = await helper.blogsInDb();
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
-
-    const titles = blogsAtEnd.map((b) => b.title);
-    assert.ok(!titles.includes(blogToDelete.title));
-  });
-
-  test("a blog post can have its likes updated", async () => {
-    const blogsAtStart = await helper.blogsInDb();
-    const blogToUpdate = blogsAtStart[0];
-
-    const updatedBlogData = {
-      title: blogToUpdate.title,
-      author: blogToUpdate.author,
-      url: blogToUpdate.url,
-      likes: blogToUpdate.likes + 10,
-    };
-
-    const response = await api
-      .put(`/api/blogs/${blogToUpdate.id}`)
-      .send(updatedBlogData)
-      .expect(200)
-      .expect("Content-Type", /application\/json/);
-
-    assert.strictEqual(response.body.likes, blogToUpdate.likes + 10);
-
-    const blogsAtEnd = await helper.blogsInDb();
-    const finalBlog = blogsAtEnd.find((b) => b.id === blogToUpdate.id);
-    assert.strictEqual(finalBlog.likes, blogToUpdate.likes + 10);
-  });
-
-  after(async () => {
-    await mongoose.connection.close();
-  });
-});
+after(async () => {
+  await mongoose.connection.close()
+})

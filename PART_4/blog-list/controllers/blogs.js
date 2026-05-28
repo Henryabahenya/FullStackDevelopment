d388aecfda2e0171f2c8c42f1d50a7166895ee22
Henry Abahenya <abahenyahadre.com>
2026-05-28 12:12:49 +0300

    4.23*: Blog List Expansion, step 11

--- PART_4/blog-list/controllers/blogs.js
+++ PART_4/blog-list/controllers/blogs.js
@@ -1,10 +1,8 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
-const middleware = require('../utils/middleware'); // Used to pull userExtractor
+const middleware = require('../utils/middleware'); 
 
-// --- GET ROUTE ---
-// Remains wide open to the public; no token or user extractor required.
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({})
@@ -13,8 +11,7 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-// --- POST ROUTE ---
-// Chained with middleware.userExtractor to capture request.user automatically
+
 blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   const { title, author, url, likes } = request.body
 
@@ -22,7 +19,7 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
     return response.status(400).end()
   }
 
-  // Look how clean! The middleware already found and attached our full user document
+ 
   const user = request.user
 
   const blog = new Blog({
@@ -30,7 +27,7 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
     author,
     url,
     likes: likes || 0,
-    user: user._id // Safe reference to the user ID
+    user: user._id 
   })
 
   const savedBlog = await blog.save()
@@ -41,30 +38,27 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-// --- DELETE ROUTE ---
-// Chained with middleware.userExtractor to guard the resource against non-creators
-blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
-  const user = request.user // Populated automatically via the middleware chain
 
-  // 1. Find the target blog post to check ownership
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
+  const user = request.user 
   const blog = await Blog.findById(request.params.id)
   if (!blog) {
     return response.status(404).json({ error: 'blog not found' })
   }
 
-  // 2. Compare the creator's ID with our verified request user ID
+  
   if (blog.user.toString() !== user._id.toString()) {
     return response.status(403).json({ 
       error: 'permission denied: only the creator can delete this blog' 
     })
   }
 
-  // 3. Authorized: Safe to delete
+  
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
 
-// --- PUT ROUTE ---
+
 blogsRouter.put('/:id', async (request, response) => {
   const { title, author, url, likes, user } = request.body
 

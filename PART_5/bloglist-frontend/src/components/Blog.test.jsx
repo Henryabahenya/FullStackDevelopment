7532c8e0f2244e9be1f925ce66c1a6be22845a55
Henry Abahenya <abahenyahadre.com>
2026-07-06 16:05:32 +0300

    5.14: blog list tests, step 2 complete with passing event simulations

--- PART_5/bloglist-frontend/src/components/Blog.test.jsx
+++ PART_5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,6 @@
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
 test("renders title and author, but does not render URL or likes by default", () => {
@@ -19,4 +21,27 @@ test("renders title and author, but does not render URL or likes by default", ()
   expect(screen.getByText(/Test Title Test Author/i)).toBeDefined();
   expect(screen.queryByText(blog.url)).toBeNull();
   expect(screen.queryByText(/likes 10/i)).toBeNull();
+});
+
+test("renders URL and number of likes when the view button is clicked", async () => {
+  const blog = {
+    title: "Test Title",
+    author: "Test Author",
+    url: "https://example.com",
+    likes: 10,
+    user: {
+      username: "testuser",
+      name: "Test User",
+      id: "user123",
+    },
+  };
+
+  render(<Blog blog={blog} />);
+  const user = userEvent.setup();
+
+  const button = screen.getByText(/view/i);
+  await user.click(button);
+
+  expect(screen.getByText(blog.url)).toBeInTheDocument();
+  expect(screen.getByText(/likes 10/i)).toBeInTheDocument();
 });

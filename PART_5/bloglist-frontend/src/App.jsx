41f40fe84e170394fcc72081b5493217e8a40579
Henry Abahenya <abahenyahadre.com>
2026-07-06 15:30:18 +0300

    5.7: blog list frontend, step 10 complete

--- PART_5/bloglist-frontend/src/App.jsx
+++ PART_5/bloglist-frontend/src/App.jsx
@@ -141,9 +141,11 @@ const App = () => {
         <BlogForm createBlog={addBlog} />
       </Togglable>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} updateBlog={handleLikeBlog} />
-      ))}
+      {[...blogs]
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} updateBlog={handleLikeBlog} />
+        ))}
     </div>
   );
 };

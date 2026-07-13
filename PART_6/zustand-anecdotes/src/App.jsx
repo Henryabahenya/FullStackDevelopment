8f998cab4e0ccfcdc879b1afe1267ff2aac7238e
Henry Abahenya <abahenyahadre.com>
2026-07-13 11:02:05 +0300

    6.2: anecdotes, step1 - implemented voting with atomic selectors to prevent React 19 infinite loop

---
+++ PART_6/zustand-anecdotes/src/App.jsx
@@ -0,0 +1,36 @@
+import { useAnecdotes, useVoteAnecdote } from "./store";
+
+const App = () => {
+  const anecdotes = useAnecdotes();
+  const voteAnecdote = useVoteAnecdote();
+
+  const vote = (id) => {
+    voteAnecdote(id);
+  };
+
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {sortedAnecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;

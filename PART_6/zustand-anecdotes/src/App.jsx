045c698ad74aeda7d728b379fa438a11703c2544
Henry Abahenya <abahenyahadre.com>
2026-07-13 11:32:00 +0300

    6.7: anecdotes, step6 - added db.json and integrated Fetch API to load initial state from backend on startup

--- PART_6/zustand-anecdotes/src/App.jsx
+++ PART_6/zustand-anecdotes/src/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
+import { useSetAnecdotes } from "./store";
 
 const App = () => {
+  const setAnecdotes = useSetAnecdotes();
+
+  useEffect(() => {
+    const fetchAnecdotes = async () => {
+      const response = await fetch("http://localhost:3001/anecdotes");
+      const data = await response.json();
+      setAnecdotes(data);
+    };
+
+    fetchAnecdotes();
+  }, [setAnecdotes]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- PART_6/zustand-anecdotes/src/store.js
+++ PART_6/zustand-anecdotes/src/store.js
@@ -1,14 +1,4 @@
 import { create } from "zustand";
-import { useShallow } from "zustand/react/shallow";
-
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
 
 const getId = () => (100000 * Math.random()).toFixed(0);
 
@@ -19,7 +9,7 @@ const asObject = (anecdote) => ({
 });
 
 const useAnecdoteStore = create((set) => ({
-  anecdotes: anecdotesAtStart.map(asObject),
+  anecdotes: [],
   filter: "",
   voteAnecdote: (id) =>
     set((state) => ({
@@ -32,6 +22,10 @@ const useAnecdoteStore = create((set) => ({
   createAnecdote: (content) =>
     set((state) => ({
       anecdotes: state.anecdotes.concat(asObject(content)),
+    })),
+  setAnecdotes: (anecdotes) =>
+    set(() => ({
+      anecdotes,
     })),
   setFilter: (query) =>
     set(() => ({
@@ -46,6 +40,9 @@ export const useVoteAnecdote = () =>
 
 export const useCreateAnecdote = () =>
   useAnecdoteStore((state) => state.createAnecdote);
+
+export const useSetAnecdotes = () =>
+  useAnecdoteStore((state) => state.setAnecdotes);
 
 export const useFilter = () => useAnecdoteStore((state) => state.filter);
 

859cf452a424fa6e14a29eab693df6407d5d42db
Henry Abahenya <abahenyahadre.com>
2026-07-15 14:36:19 +0300

    7.8: Added ErrorBoundary component to catch rendering errors and wrapped route views while keeping navigation visible

--- PART_7/routed-anecdotes/src/App.jsx
+++ PART_7/routed-anecdotes/src/App.jsx
@@ -4,6 +4,7 @@ import AnecdoteList from "./components/AnecdoteList";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import CreateNew from "./components/CreateNew";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const App = () => {
   return (
@@ -11,11 +12,13 @@ const App = () => {
       <div>
         <h1>Software anecdotes</h1>
         <Menu />
-        <Routes>
-          <Route path="/" element={<AnecdoteList />} />
-          <Route path="/create" element={<CreateNew />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <ErrorBoundary>
+          <Routes>
+            <Route path="/" element={<AnecdoteList />} />
+            <Route path="/create" element={<CreateNew />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </ErrorBoundary>
         <Footer />
       </div>
     </Router>

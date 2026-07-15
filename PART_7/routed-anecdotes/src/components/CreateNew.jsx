e58cab1ffa2172d7ce573c90db4e6174fa318de4
Henry Abahenya <abahenyahadre.com>
2026-07-15 13:16:51 +0300

    7.6: Anecdotes - Resolved input spread issue by destructuring reset from DOM props

--- PART_7/routed-anecdotes/src/components/CreateNew.jsx
+++ PART_7/routed-anecdotes/src/components/CreateNew.jsx
@@ -2,26 +2,26 @@ import { useNavigate } from "react-router-dom";
 import { useField } from "../hooks";
 
 const CreateNew = ({ addAnecdote }) => {
-  const content = useField("text");
-  const author = useField("text");
-  const info = useField("text");
+  const { reset: resetContent, ...contentProps } = useField("text");
+  const { reset: resetAuthor, ...authorProps } = useField("text");
+  const { reset: resetInfo, ...infoProps } = useField("text");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     addAnecdote({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: contentProps.value,
+      author: authorProps.value,
+      info: infoProps.value,
       votes: 0,
     });
     navigate("/");
   };
 
   const handleReset = () => {
-    content.reset();
-    author.reset();
-    info.reset();
+    resetContent();
+    resetAuthor();
+    resetInfo();
   };
 
   return (
@@ -30,15 +30,15 @@ const CreateNew = ({ addAnecdote }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name="content" {...content} />
+          <input name="content" {...contentProps} />
         </div>
         <div>
           author
-          <input name="author" {...author} />
+          <input name="author" {...authorProps} />
         </div>
         <div>
           url for more info
-          <input name="info" {...info} />
+          <input name="info" {...infoProps} />
         </div>
         <button>create</button>
         <button type="button" onClick={handleReset}>

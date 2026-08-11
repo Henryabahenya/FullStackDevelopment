ae9c40c978f3a17f742262b7fc6bf7d7f8ac4960
Henry Abahenya <abahenyahadre.com>
2026-08-11 15:22:34 +0300

    Complete Part 8 exercise 8.5 (allBooks query with optional author and genre filters)

--- PART_8/fs-graphql/library-backend/index.js
+++ PART_8/fs-graphql/library-backend/index.js
@@ -114,7 +114,7 @@ const typeDefs = `
   }
 
   type Query {
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
     bookCount: Int!
     authorCount: Int!
@@ -124,11 +124,21 @@ const typeDefs = `
 const resolvers = {
   Query: {
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books;
+      let filteredBooks = books;
+
+      if (args.author) {
+        filteredBooks = filteredBooks.filter(
+          (book) => book.author === args.author,
+        );
       }
 
-      return books.filter((book) => book.author === args.author);
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter((book) =>
+          book.genres.includes(args.genre),
+        );
+      }
+
+      return filteredBooks;
     },
     allAuthors: () => authors,
     bookCount: () => books.length,

6942101d3e186e6ddc04e8cfa16f5fd821c9a76a
Henry Abahenya <abahenyahadre.com>
2026-08-25 16:21:39 +0300

    Complete Exercise 9.6: Configure ESLint and pass lint checks

--- PART_9/healthapp/index.ts
+++ PART_9/healthapp/index.ts
@@ -1,9 +1,28 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator.ts";
 
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
+
+  if (!height || !weight || Array.isArray(height) || Array.isArray(weight)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const h = Number(height);
+  const w = Number(weight);
+
+  if (isNaN(h) || isNaN(w)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const bmi = calculateBmi(h, w);
+  return res.json({ weight: w, height: h, bmi });
 });
 
 const PORT = process.env.PORT || 3003;

d2d6b7f942110a43c906497c8dffe4a8a1db69ac
Henry Abahenya <abahenyahadre.com>
2026-08-28 16:04:12 +0300

    Complete Exercise 9.18: Course, part 2

---
+++ PART_9/course/src/diaryService.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export interface DiaryEntry {
+  id: number;
+  date: string;
+  visibility: string;
+  weather: string;
+  comment?: string;
+}
+
+const BASE_URL = "http://localhost:3000/api/diaries";
+
+export const diaryService = {
+  getAll: async () => {
+    const response = await axios.get<DiaryEntry[]>(BASE_URL);
+    return response.data;
+  },
+};

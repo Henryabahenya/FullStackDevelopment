83db26997570c2c9329589636535ea20bc2268d7
Henry Abahenya <abahenyahadre.com>
2026-08-28 11:39:49 +0300

    Complete Exercise 9.8: Configure E2E tests and workflow

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -57,7 +57,7 @@ app.post("/exercises", (req, res) => {
   return res.json(result);
 });
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

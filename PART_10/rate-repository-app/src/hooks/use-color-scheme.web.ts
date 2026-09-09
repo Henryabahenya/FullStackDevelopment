b5098f0b67f0d96f072b344ca5647ba771e159f8
Henry Abahenya <abahenyahadre.com>
2026-09-09 13:32:12 +0300

    Complete Part 10 Exercise 2: Set up ESLint and fix template hook lint error

--- PART_10/rate-repository-app/src/hooks/use-color-scheme.web.ts
+++ PART_10/rate-repository-app/src/hooks/use-color-scheme.web.ts
@@ -1,21 +1,14 @@
-import { useEffect, useState } from 'react';
 import { useColorScheme as useRNColorScheme } from 'react-native';
 
 /**
- * To support static rendering, this value needs to be re-calculated on the client side for web
- */
+* To support static rendering, this value needs to be re-calculated on the client side for web
+*/
 export function useColorScheme() {
-  const [hasHydrated, setHasHydrated] = useState(false);
+const colorScheme = useRNColorScheme();
 
-  useEffect(() => {
-    setHasHydrated(true);
-  }, []);
+if (typeof window === 'undefined') {
+return 'light';
+}
 
-  const colorScheme = useRNColorScheme();
-
-  if (hasHydrated) {
-    return colorScheme;
-  }
-
-  return 'light';
+return colorScheme;
 }
